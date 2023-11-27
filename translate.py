abf551459bb3debc6c49aac66578c22a95e611d5
Jelmer <jelmer.neeven@gmail.com>
2023-11-27 21:53:35 +0200

    Full version working!

--- translate.py
+++ translate.py
@@ -5,18 +5,19 @@ from pathlib import Path
 import deepl
 import genanki
 import googletrans
+from concurrent.futures import ThreadPoolExecutor
 from gtts import gTTS
 from tqdm import tqdm
+import math
 
 # These are the only ones I'm interested for now, but both Deepl and
 # Google Translate offer many options.
 LANGUAGE_NAMES = {
     "EL": "Greek",
-    "ES": "Spanish",
-    "PT-PT": "Portuguese",
-    "PT": "Portuguese",
-    "NL": "Dutch",
     "EN": "English",
+    "ES": "Spanish",
+    "NL": "Dutch",
+    "PT": "Portuguese",
 }
 
 
@@ -25,7 +26,10 @@ def load_vocab(filepath: Path) -> tuple[dict[int, str], dict[int, list[str]]]:
     input_vocab = filepath.read_text().strip().splitlines()
 
     vocab_dict, tag_dict = {}, {}
-    for line in input_vocab[:5]:
+    for line in input_vocab:
+        # Allow for comments
+        if line.startswith("#"):
+            continue
         index, phrase, *tags = line.strip().split("\t")
         vocab_dict[int(index)] = phrase
         tag_dict[int(index)] = tags
@@ -43,18 +47,21 @@ def translate_deepl(
     language using Deepl.
     Uses a Deepl API key that should be stored at `.deepl_auth`.
     """
-    deepl_translator = deepl.Translator(
-        auth_key=Path(".deepl_auth").read_text().strip()
-    )
-
     output_vocab = {}
-    for id, phrase in tqdm(input_vocab.items(), desc="Obtaining Deepl translations..."):
+
+    # Separate function to translate a single phrase so we can multithread
+    def translate_phrase(args):
+        id, phrase = args
+
+        deepl_translator = deepl.Translator(
+            auth_key=Path(".deepl_auth").read_text().strip()
+        )
+
         deepl_result = deepl_translator.translate_text(
             phrase,
             source_lang=source_language,
             target_lang=target_language,
             split_sentences=0,
-            # formality="prefer_more", # or "prefer_less"
         )
 
         verification_result = deepl_translator.translate_text(
@@ -66,6 +73,15 @@ def translate_deepl(
 
         output_vocab[id] = (deepl_result.text, verification_result.text)
 
+    with ThreadPoolExecutor() as executor:
+        for _ in tqdm(
+            executor.map(translate_phrase, input_vocab.items()),
+            desc="Obtaining Deepl translations...",
+            total=len(input_vocab)
+        ):
+            # We only loop to get a nice progress bar
+            pass
+
     return output_vocab
 
 
@@ -73,14 +89,26 @@ def translate_google(
     input_vocab: dict[int, str], target_language: str, source_language: str = "EN"
 ) -> dict[int, str]:
     """Batch-translates the entire input vocabulary to the target language using
-    Google Translate. NOTE: I do not know if there is any practical limit on batch size."""
+    Google Translate."""
     google_translator = googletrans.Translator()
-    print("Obtaining Google translations...")
-    translations = google_translator.translate(
-        list(input_vocab.values()),
-        source=source_language.lower(),
-        dest=target_language.lower(),
-    )
+    values = list(input_vocab.values())
+    batch_size = 20
+    start_idx = 0
+    translations = []
+
+    # Translate in batches of 20 phrases and add outputs to translations list
+    for start_idx in tqdm(
+        range(0, len(values), batch_size),
+        desc="Obtaining batch Google translations...",
+        total=math.ceil(len(values) / batch_size)
+    ):
+        batch_translations = google_translator.translate(
+            values[start_idx:start_idx + batch_size],
+            source=source_language.lower(),
+            dest=target_language.lower(),
+        )
+        translations.extend(batch_translations)
+        start_idx += batch_size
 
     return {
         id: translation.text
@@ -121,77 +149,13 @@ def get_pronunciations(
 
     mp3_paths = {}
     # TODO: add timeout to prevent rate-limit and IP block?
-    for id, phrase in tqdm(vocab.items(), desc="Obtaining pronunciations..."):
+    for id, phrase in tqdm(vocab.items(), desc="Obtaining pronunciations...", total=len(vocab)):
         speak = gTTS(text=phrase, lang=lang, slow=False)
         path = str(output_dir.joinpath(f"{id}.mp3"))
         speak.save(path)
         mp3_paths[id] = path
 
     return mp3_paths
-
-
-def main():
-    # Change these according to your needs
-    target_language = "EL"
-    verification_language = "NL"
-    source_language = "EN"
-    input_vocab, tags = load_vocab(Path("Vocabs", "english.csv"))
-
-    output_dir = Path("Output")
-    output_dir.mkdir(exist_ok=True)
-
-    # Do the translation work
-    deepl_output = translate_deepl(
-        input_vocab,
-        target_language=target_language,
-        source_language=source_language,
-        verification_language=verification_language,
-    )
-    google_output = translate_google(
-        input_vocab, target_language=target_language, source_language=source_language
-    )
-
-    # Postprocess results
-    results = {}
-    assert len(deepl_output) == len(google_output)
-    for id, (deepl_translation, deepl_verification) in deepl_output.items():
-        translation, verification = process_translations(
-            deepl_translation, google_output[id], deepl_verification
-        )
-
-        results[id] = {
-            source_language: input_vocab[id],
-            target_language: translation,
-            verification_language: verification,
-            "tags": tags[id],
-        }
-
-    # Save JSON output before moving on to pronunciations, since the translations are the bottleneck.
-    output_dir.joinpath("output.json").write_text(json.dumps(results, indent="\t"))
-    print(f"Intermediate outputs saved in {(str(output_dir))}.")
-
-    # Obtain pronunciation sound files and add them to results dict.
-    pronunciations = get_pronunciations(
-        {k: v[target_language] for k, v in results.items()}, language=target_language
-    )
-    for id, p_file in pronunciations.items():
-        results[id]["pronunciation_file"] = p_file
-
-    # Update JSON with the newly added pronunciation files before moving on to Anki deck creation.
-    # The JSON is easier to inspect and could be useful for non-Anki users as well.
-    output_dir.joinpath("output.json").write_text(json.dumps(results, indent="\t"))
-
-    # Finally, create the actual Anki deck and save it to the output folder as well.
-    create_anki_deck(
-        results,
-        target_language=target_language,
-        source_language=source_language,
-        verification_language=verification_language,
-        add_reverse_cards=True,
-        output_dir=output_dir,
-    )
-
-    print(f"Done! JSON and Anki deck saved to {str(output_dir)}.")
 
 
 def create_anki_deck(
@@ -276,6 +240,70 @@ def create_anki_deck(
     package = genanki.Package(deck)
     package.media_files = [v["pronunciation_file"] for v in translated_vocab.values()]
     package.write_to_file(output_dir.joinpath("output.apkg"))
+
+
+def main():
+    # Change these according to your needs. TODO: make CLI
+    target_language = "EL"
+    verification_language = "NL"
+    source_language = "EN"
+    input_vocab, tags = load_vocab(Path("Vocabs", "english.csv"))
+
+    output_dir = Path("Output")
+    output_dir.mkdir(exist_ok=True)
+
+    # Do the translation work
+    google_output = translate_google(
+        input_vocab, target_language=target_language, source_language=source_language
+    )
+    deepl_output = translate_deepl(
+        input_vocab,
+        target_language=target_language,
+        source_language=source_language,
+        verification_language=verification_language,
+    )
+
+    # Postprocess results
+    results = {}
+    assert len(deepl_output) == len(google_output)
+    for id, (deepl_translation, deepl_verification) in deepl_output.items():
+        translation, verification = process_translations(
+            deepl_translation, google_output[id], deepl_verification
+        )
+
+        results[id] = {
+            source_language: input_vocab[id],
+            target_language: translation,
+            verification_language: verification,
+            "tags": tags[id],
+        }
+
+    # Save JSON output before moving on to pronunciations, since the translations are the bottleneck.
+    output_dir.joinpath("output.json").write_text(json.dumps(results, indent="\t"))
+    print(f"Intermediate outputs saved in {(str(output_dir))}.")
+
+    # Obtain pronunciation sound files and add them to results dict.
+    pronunciations = get_pronunciations(
+        {k: v[target_language] for k, v in results.items()}, language=target_language
+    )
+    for id, p_file in pronunciations.items():
+        results[id]["pronunciation_file"] = p_file
+
+    # Update JSON with the newly added pronunciation files before moving on to Anki deck creation.
+    # The JSON is easier to inspect and could be useful for non-Anki users as well.
+    output_dir.joinpath("output.json").write_text(json.dumps(results, indent="\t"))
+
+    # Finally, create the actual Anki deck and save it to the output folder as well.
+    create_anki_deck(
+        results,
+        target_language=target_language,
+        source_language=source_language,
+        verification_language=verification_language,
+        add_reverse_cards=True,
+        output_dir=output_dir,
+    )
+
+    print(f"Done! JSON and Anki deck saved to {str(output_dir)}.")
 
 
 if __name__ == "__main__":
